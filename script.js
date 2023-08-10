e4f559c7afa362662ba85ce3c79163de3dfffe90
chask96969 <160220a009@gmail.com>
2023-08-10 13:44:29 +0530

    success

---
+++ script.js
@@ -0,0 +1,31 @@
+const API_KEY = 'AIzaSyAj606So8owXHI4hY8Z-Sknwpt9nQtmPKs';
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+const resultContainer = document.getElementById('resultContainer');
+
+searchButton.addEventListener('click', async () => {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== '') {
+        try {
+            const response = await fetch(`${BASE_URL}?q=${searchTerm}&key=${API_KEY}`);
+            const data = await response.json();
+            const totalResultsElement = document.createElement('p');
+            if (data.items) {
+                const pageInfo = data.pageInfo;
+                totalResultsElement.textContent = `It has been searched for ${pageInfo.totalResults} times`;
+            }
+            else {
+                totalResultsElement.textContent = 'Seems like you have exceeded the daily limit';
+            }
+            totalResultsElement.classList.add('result'); 
+            resultContainer.innerHTML = '';
+            resultContainer.appendChild(totalResultsElement);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+});
+
+
